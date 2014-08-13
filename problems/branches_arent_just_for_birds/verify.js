cab0c0c2174a005531729c6ef60e51d4fcf04874
Jessica Lord <jessica.lord@github.com>
2014-08-13 15:36:28 -0700

    start new logic

--- problems/branches_arent_just_for_birds/verify.js
+++ problems/branches_arent_just_for_birds/verify.js
@@ -4,23 +4,45 @@ var exec = require('child_process').exec
 
 // get their username
 // verify branch matches username
+// // verify that they've put their file in the right dir
 // verify they've pushed
 
 exec('git config user.username', function(err, stdout, stdrr) {
-  var username = stdout.trim().toLowerCase()
-  
+  var username = stdout.trim()
+
   exec('git status', function(err, stdout, stderr) {
     var branch = stdout.trim().toLowerCase()
-    var branchName = "add-" + username
+    var branchName = "add-" + username.toLowerCase()
     exec('git reflog show origin/' + branchName, function(err, stdout, stderr) {
-      
+
       var ref = stdout.trim()
-      if (branch.match(branchName)) console.log("Found the branch as expected!")
-      else console.log("branch name was not:\n'" + branchName + "'")
-      
+      if (branch.match(branchName)) {
+        // branch name is correct
+        // check file dir is correct
+        exec('git log --name-only --oneline', function(err, stdout, stdrr) {
+          if (!stdout.match('contributors/')) {
+            // it's NOT in right dir
+            return console.log("Oops, looks like your file isn't in\n" +
+            "the 'contributors' folder! Move it and commit again.")
+          } else {
+            if (stdout.toLowerCase().match('add-' + username.toLowerCase())) {
+              // filename is correct
+              console.log("Changes have been committed!")
+            } else {
+              console.log("Filename was expected to be\nadd-" + username)
+            }
+          }
+        })
+
+        console.log("Found the branch as expected!")
+
+      } else {
+        console.log("branch name was not:\n'" + branchName + "'")
+      }
+
       if (ref.match("update by push")) console.log("Changes have been pushed!")
       else console.log("Changes not pushed")
-      
+
     })
   })
 })
